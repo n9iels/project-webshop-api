ab094a5490a2d52affc5a63211c96f6694c75fe7
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2016-10-27 21:54:32 +0200

    Use connection pool

--- helpers/database.js
+++ helpers/database.js
@@ -8,7 +8,7 @@ var fs    = require('fs');
 * @return {void}
 */
 function Database() {
-    this._connection = mysql.createConnection({
+    this._pool = mysql.createPool({
         host     : '84.84.245.29',
         user     : 'webshop',
         password : 'HQv@1rM0KUXXjpZw09WE',
@@ -28,13 +28,18 @@ function Database() {
 */
 Database.prototype.executeQuery = function (sql, params, callback)
 {
-    this._connection.query(sql, params, function (error, results, fields) {
-        if (error) {
-            fs.appendFile("./log/error.log", "Error when execute query '" + sql + "'\n" + error.message + "\n\n");
-            console.error('error when execute query: ' + error.message);
-        }
+    this._pool.getConnection(function(err, connection)
+    {
+        connection.query(sql, params, function (error, results, fields)
+        {
+            if (error) {
+                fs.appendFile("./log/error.log", "Error when execute query '" + sql + "'\n" + error.message + "\n\n");
+                console.error('error when execute query: ' + error.message);
+            }
 
-        return callback(results);
+            callback(results);
+            connection.release();
+        });
     });
 }
 
