684263a2eecd28fb9c2989fb4994d4192916ee06
MisterHabbini1001 <0910681@hr.nl>
2016-10-28 15:41:24 +0200

    Connection to Game database
    Replaced User with Game in the execute Query

--- endpoints/products.js
+++ endpoints/products.js
@@ -1,5 +1,5 @@
 var Authenticate   = require('../helpers/authenticate');
-var DatabaseHelper =  require('../helpers/database');
+var DatabaseHelper = require('../helpers/database');
 var Database       = new DatabaseHelper();
 var Products       = {};
 
@@ -11,14 +11,26 @@ var Products       = {};
  *
  * @return {void}
  */
+
+/**
+ * STEP 1: Dingen wijzigen en opslaan
+ * STEP 2: Ga naar command line
+ * STEP 3: Ctrl + C om huidige proces af te sluiten
+ * STEP 4: Voer node index.js in als commando (of druk pijl omhoog om meest recente commando te krijgen en voer deze uit)
+ * STEP 5: Ga naar Chrome browser en voer localhost:8080/(naam van endpoint) (BIJVOORBEELD localhost:8080/products)
+ * 
+ * VOORBEELD QUERY: http://localhost:8080/products?genre=actie&price=50-90&name=battlefield
+ * VOORBEELD QUERY 2: http://localhost:8080/products
+ */
 Products.init = function(server)
 {
     // Endpoint for '/products' to receive all products in the database
     server.get('products', function (req, res, next)
     {
         var query = req.query;
+        console.log(query);
 
-        Database.executeQuery("SELECT * FROM User", [], function (result)
+        Database.executeQuery("SELECT * FROM Game", [], function (result)
         {
             if (result)
             {
