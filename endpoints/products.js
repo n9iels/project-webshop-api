addd3816d49ae8095e94e051e3e445c06dd5f86c
MisterHabbini1001 <0910681@hr.nl>
2016-11-03 11:59:35 +0100

    Added empty string product filters
    Self explanatory title

--- endpoints/products.js
+++ endpoints/products.js
@@ -25,26 +25,8 @@ var Products       = {};
  */
 Products.init = function(server)
 {
-    // Endpoint for '/products' to receive all products in the database
-    server.get('products', function (req, res, next)
-    {
-        var query = req.query;
-
-        Database.executeQuery("SELECT * FROM game g, platform_independent_info pii WHERE g.pi_id = pii.pi_id", [], function (result)
-        {
-            if (result)
-            {
-                return res.send(result)
-            }
-
-            return res.send({message:"No results!"})
-        });
-
-        next();
-    });
-
     // Endpoint for '/products/filter' to receive filtered products from the database
-    server.get('products/filter', function (req, res, next)
+    server.get('products', function (req, res, next)
     {
         var query = req.query;
 
@@ -69,20 +51,20 @@ Products.init = function(server)
 
         var base_sql = "SELECT * FROM game g INNER JOIN platform_independent_info pi ON g.pi_id = pi.pi_id WHERE 1=1 ";
         
-        if (ean_number != null) { base_sql += "AND ean_number = " + ean_number + " "; }
+        if (ean_number != null && ean_number != "") { base_sql += "AND ean_number = " + ean_number + " "; }
 
-        if (platform != null) { base_sql += "AND platform = '" + platform + "' "; }
-        if (release_date != null) { base_sql += "AND release_date = '" + release_date + "' "; }
-        if (pegi_age != null) { base_sql += "AND pegi_age = '" + pegi_age + "' "; }
-        if (stock != null) { base_sql += "AND stock = '" + stock + "' "; }
-        if (price1 != null && price2 != null) { base_sql += "AND price BETWEEN " + price1 + " AND " + price2 + " "; }
+        if (platform != null && platform != "") { base_sql += "AND platform = '" + platform + "' "; }
+        if (release_date != null && release_date != "") { base_sql += "AND release_date = '" + release_date + "' "; }
+        if (pegi_age != null && pegi_age != "") { base_sql += "AND pegi_age = '" + pegi_age + "' "; }
+        if (stock != null && stock != "") { base_sql += "AND stock = '" + stock + "' "; }
+        if (price1 != null && price2 != null && price1 != "" && price2 != "") { base_sql += "AND price BETWEEN " + price1 + " AND " + price2 + " "; }
 
-        if (publisher != null) { base_sql += "AND publisher = '" + publisher + "' "; }
-        if (title != null) { base_sql += "AND title = '" + title + "' "; }
-        if (subtitle != null) { base_sql += "AND subtitle = '" + subtitle + "' "; }
-        if (genre != null) { base_sql += "AND genre = '" + genre + "' "; }
-        if (franchise != null) { base_sql += "AND franchise = '" + franchise + "' "; }
-        if (description != null) { base_sql += "AND description = '" + description + "' "; }
+        if (publisher != null && publisher != "") { base_sql += "AND publisher = '" + publisher + "' "; }
+        if (title != null && title != "") { base_sql += "AND title = '" + title + "' "; }
+        if (subtitle != null && subtitle != "") { base_sql += "AND subtitle = '" + subtitle + "' "; }
+        if (genre != null && genre != "") { base_sql += "AND genre = '" + genre + "' "; }
+        if (franchise != null && franchise != "") { base_sql += "AND franchise = '" + franchise + "' "; }
+        if (description != null && description != "") { base_sql += "AND description = '" + description + "' "; }
 
         Database.executeQuery(base_sql, [], function (result)
           {
