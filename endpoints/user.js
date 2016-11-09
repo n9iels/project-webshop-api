a20413928decf6738579b045e8cc15ccc12984e4
MisterHabbini1001 <0910681@hr.nl>
2016-11-09 09:37:41 +0100

    Register can now insert address and sh*t
    guiuguiuguiuguiuguittttt

--- endpoints/user.js
+++ endpoints/user.js
@@ -86,13 +86,12 @@ User.init = function(server, database)
     {
         try
         {
-            var post = req.body;
+            var post = JSON.parse(req.body);
 
             // Get user id, new password, repeated password, email, secret question and the answer to the secret question
-            var user_id = post.user_id; 
             var new_password = post.new_password;
             var repeat_password = post.repeat_password;
-            var e_mail = post.e_mail;
+            var e_mail = post.email;
             var secret_question = post.secret_question;
             var secret_question_answer = post.secret_question_answer;
         }
@@ -101,17 +100,23 @@ User.init = function(server, database)
             res.send(422, "Missing fields")
         }
         
-        database.executeQuery("SELECT * FROM user WHERE user_id = ? AND email = ? AND secret_question = ? AND secret_question_answer = ?", [user_id, email, secret_question, secret_question_answer], function (result)
+        database.executeQuery("SELECT * FROM user WHERE email = ? AND secret_question = ? AND secret_question_answer = ?", [e_mail, secret_question, secret_question_answer], function (result, error)
         {
-            if (result.length == 0)
+            //console.log(e_mail, secret_question, secret_question_answer)
+            if (error)
             {
-                res.send(401)
+                res.send(422, "There are missing fields or the email allready exists")
             }
         });
 
-        database.executeQuery("UPDATE user SET password = ? WHERE user_id = ?", [new_password, user_id], function (result)
+        database.executeQuery("UPDATE user SET password = ? WHERE email = ?", [new_password, e_mail], function (result)
         {
-          res.send("The password for the user has been successfully reset")
+            if (error)
+            {
+                res.send(422, "There are missing fields or the email allready exists")
+            }
+
+            res.send("The password for the user has been successfully reset")
         });
 
         next();
@@ -122,32 +127,53 @@ User.init = function(server, database)
         try
         {
             var post = JSON.parse(req.body);
+            console.log(post);
 
-            // Get e-mail, password, first_name, surname, gender, date_of_birth, phone_number, secret_question and secret_question_answer
+            // Get e-mail, password, first_name, insertion, surname, gender, date_of_birth, phone_number, secret_question and secret_question_answer
             var e_mail = post.e_mail;
             var password = post.password;
             var first_name = post.first_name;
+            var insertion = post.insertion;
             var surname = post.surname;
             var gender = post.gender;
             var date_of_birth = post.date_of_birth;
             var phone_number = post.phone_number;
             var secret_question = post.secret_question;
             var secret_question_answer = post.secret_question_answer;
+
+            // Get street_name, number, postal_code and city
+            var postal_code = post.postal_code;
+            var house_number = post.number;
+            var street_name = post.street_name;
+            var city = post.city;
         }
         catch (err)
         {
             res.send(422, "Missing fields")
         }
        
-        database.executeQuery("INSERT INTO user (email, password, first_name, surname, gender, date_of_birth, phone_number, secret_question, secret_question_answer) VALUES (?,?,?,?,?,?,?,?,?)", [e_mail, password, first_name, surname, gender, date_of_birth, phone_number, secret_question, secret_question_answer], function (result, error)
+        database.executeQuery("INSERT INTO user (email, password, first_name, insertion, surname, gender, date_of_birth, phone_number, secret_question, secret_question_answer) VALUES (?,?,?,?,?,?,?,?,?,?)", [e_mail, password, first_name, insertion, surname, gender, date_of_birth, phone_number, secret_question, secret_question_answer], function (result, error) // insertion is not filled in
         {           
             if (error)
             {
                 res.send(422, "There are missing fields or the email allready exists")
             }
             
+            //res.send("Your user information has been stored in the database")
+        });
+        console.log("first query done.");
+
+        database.executeQuery("INSERT INTO address (postal_code, house_number, user_id, street, city) VALUES (?,?,(SELECT user_id FROM user WHERE email = ?),?,?)", [postal_code, house_number, e_mail, street_name, city], function (result, error) // Query doesn't insert anything at all
+        {
+            //console.log(postal_code, house_number, e_mail, street_name, city)
+            if (error)
+            {
+                res.send(422, "There are missing fields")
+            }
+
             res.send("You have been successfully registered :p")
         });
+        console.log("second query done.");
 
         next();
     });
@@ -157,3 +183,7 @@ module.exports = function (server, database)
 {
     return User.init(server, database);
 }
+
+
+
+
