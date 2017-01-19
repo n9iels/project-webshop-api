f3e88e072ae04d7da0beb244116b314248c12f07
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-19 10:00:11 +0100

    #1 Add admin endpoint

---
+++ endpoints/admin.js
@@ -0,0 +1,65 @@
+var Authenticate = require('../helpers/authenticate'); 
+ 
+var Admin = {} 
+ 
+Admin.init = function(server, database) 
+{ 
+    // Endpoint for '/admin' to get info of users 
+    server.get('admin/:email', function (req, res, next) //, Authenticate.admin 
+    { 
+        database.executeQuery("SELECT * FROM  user WHERE email = ?", [req.params.email], function(result) 
+        { 
+            if (result.length > 0) 
+            { 
+                res.send(result); 
+            } 
+            else 
+            { 
+                res.send(401, "result.length !> 0"); 
+            } 
+        }) 
+ 
+        next(); 
+    }); 
+ 
+    // Endpoint for '/users' to get user id's 
+    server.get('admin', function (req, res, next) //, Authenticate.admin 
+    { 
+        // If the post data is correctly set we can check the credentials 
+        database.executeQuery("SELECT first_name, insertion, surname, email, phone_number, user_type FROM user", [], function (result) 
+        { 
+            if (result.length > 0) 
+            { 
+                res.send(result); 
+            } 
+            else 
+            { 
+                res.send(401, "result.length !> 0"); 
+            } 
+        }); 
+    }); 
+ 
+    // Endpoint for '/delete_user' for deleting user by admin 
+    server.get('admin/d/delete_user', Authenticate.admin, function (req, res, next) 
+    { 
+        // delete user from relevant tables 
+        database.executeQuery("SELECT first_name, insertion, surname, email, phone_number, user_type FROM user", [], function (result) 
+        { 
+            if (result.length > 0) 
+            { 
+                res.send(result); 
+            } 
+            else 
+            { 
+                res.send(401, "result.length !> 0"); 
+            } 
+        }); 
+    }); 
+ 
+} 
+ 
+ 
+module.exports = function (server, database) 
+{ 
+    return Admin.init(server, database); 
+}
