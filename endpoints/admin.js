95634d0797c58195a7baafd0817ad37dc28ee8a3
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-19 13:23:33 +0100

    #1 update_user endpoint beginnetje

--- endpoints/admin.js
+++ endpoints/admin.js
@@ -5,7 +5,7 @@ var Admin = {}
 Admin.init = function(server, database) 
 { 
     // Endpoint for '/admin' to get info of users 
-    server.get('admin/:id', function (req, res, next) //, Authenticate.admin 
+    server.get('admin/user/:id', function (req, res, next) //, Authenticate.admin 
     { 
         database.executeQuery("SELECT * FROM  user WHERE user_id = ?", [req.params.id], function(result) 
         { 
@@ -23,7 +23,7 @@ Admin.init = function(server, database)
     }); 
  
     // Endpoint for '/users' to get user id's 
-    server.get('admin', function (req, res, next) //, Authenticate.admin 
+    server.get('admin/users', function (req, res, next) //, Authenticate.admin 
     { 
         // If the post data is correctly set we can check the credentials 
         database.executeQuery("SELECT * FROM user", [], function (result) 
@@ -38,23 +38,11 @@ Admin.init = function(server, database)
             } 
         }); 
     }); 
- 
-    // Endpoint for '/delete_user' for deleting user by admin 
-    server.get('admin/d/delete_user', Authenticate.admin, function (req, res, next) 
-    { 
-        // delete user from relevant tables 
-        database.executeQuery("SELECT first_name, insertion, surname, email, phone_number, user_type FROM user", [], function (result) 
-        { 
-            if (result.length > 0) 
-            { 
-                res.send(result); 
-            } 
-            else 
-            { 
-                res.send(401, "result.length !> 0"); 
-            } 
-        }); 
-    }); 
+
+    server.patch('admin/update_user', function(req, res, next)
+    {
+        
+    })
  
 } 
  
