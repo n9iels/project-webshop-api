84ba74917d36d9840bb69f74ae9ddc06be10d025
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2017-01-20 13:46:50 +0100

    #11 Admin can block users

--- helpers/database.js
+++ helpers/database.js
@@ -36,7 +36,6 @@ Database.prototype.executeQuery = function (sql, params, callback)
             connection.release();
             
             if (error) {
-                fs.appendFile("./log/error.log", "Error when execute query '" + sql + "'\n" + error.message + "\n\n");
                 console.error('error when execute query: ' + error.message);
             }
 
