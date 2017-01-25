00d477677c70afa0494b1c005e42c785c210ed50
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-25 22:48:50 +0100

    #21 niet based on aantal gebruikers, maar aantal in orders

--- endpoints/stats.js
+++ endpoints/stats.js
@@ -12,16 +12,18 @@ Stats.init = function(server, database)
         SET @rank_count = 0;\
         SELECT * FROM\
         (\
-            SELECT ocgt.ean_number, super_amount, CASE\
+            SELECT ocgt.title, super_amount, CASE\
                 WHEN @prev_value = super_amount THEN @rank_count\
                 WHEN @prev_value := super_amount THEN @rank_count := @rank_count + 1\
                 END AS rank\
             FROM (\
-                    SELECT ocg.ean_number, sum(amount) as super_amount\
+                    SELECT pii.title, sum(ocg.amount) as super_amount\
                     FROM orders_contain_games ocg\
-                    GROUP BY ocg.ean_number\
-                ORDER BY super_amount DESC\
-                ) as ocgt\
+                        JOIN game g ON g.ean_number = ocg.ean_number\
+                        JOIN platform_independent_info pii ON g.pi_id = pii.pi_id\
+                    GROUP BY pii.title\
+                    ORDER BY super_amount DESC\
+            ) as ocgt\
         ) as ranked\
         WHERE ranked.rank <= 10"
 
