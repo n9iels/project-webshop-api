cf593f373fe4ba93502bea542b5f2f52e39c6552
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-25 09:33:42 +0100

    #11 minor change

--- endpoints/public_wishlist.js
+++ endpoints/public_wishlist.js
@@ -16,7 +16,10 @@ PublicWishlist.init = function (server, database)
 {
     server.get('public_wishlist/:user_id', function (req, res, next)
     {
-        database.executeQuery("SELECT * FROM wishlist_items, user, game, platform_independent_info WHERE user_id = ? ", req.params.user_id, function (error, result)
+        console.log(req.params.user_id);
+        database.executeQuery("SELECT u.first_name, u.insertion, u.surname, u.is_active, w.is_public, g.image, pii.title, pii.subtitle, g.platform, pii.genre, g.price, g.stock\
+                               FROM user u JOIN wishlist w ON u.user_id = w.user_id JOIN wishlist_items wi ON w.wishlist_id = wi.wishlist_id JOIN game g ON g.ean_number = wi.ean_number JOIN platform_independent_info pii ON g.pi_id = pii.pi_id\
+                               WHERE u.user_id = ?", [req.params.user_id], function (error, result)
         {
             if(error)
             {
