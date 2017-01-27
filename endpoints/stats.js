b822a7bb7dc294750850a243b81b2f0a5a320199
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-01-27 15:01:32 +0100

    #21 date nog doen. zie gecommente code

--- endpoints/stats.js
+++ endpoints/stats.js
@@ -7,45 +7,25 @@ Stats.init = function(server, database)
     // Endpoint for '/stats/topgames' to get games bought by most users
     server.get('stats/topgames', Authenticate.admin, function (req, res, next) // NOTICE: 'stats/:month'
     { 
-        var aantal_copies_query = 
-       "SET @prev_value = NULL;\
-        SET @rank_count = 0;\
-        SELECT * FROM\
-        (\
-            SELECT piit.title, piit.subtitle, super_amount, CASE\
-                WHEN @prev_value = super_amount THEN @rank_count\
-                WHEN @prev_value := super_amount THEN @rank_count := @rank_count + 1\
-                END AS rank\
-            FROM ( \
-                SELECT pii.title, pii.subtitle, sum(ocg.amount) as super_amount\
-                FROM orders_contain_games ocg\
-                    JOIN game g ON g.ean_number = ocg.ean_number\
-                    JOIN platform_independent_info pii ON pii.pi_id = g.pi_id\
-                GROUP BY pii.title\
-                ORDER BY super_amount desc\
-            ) as piit\
-        ) as ranked\
-        WHERE ranked.rank <= 10"
+        var cur_date = new Date(); //current date
 
-        var aantal_users_query =
-       "SET @prev_value = NULL;\
-        SET @rank_count = 0;\
-        SELECT * FROM\
-        (\
-            SELECT ocgt.title, ocgt.subtitle, super_amount, CASE\
-                WHEN @prev_value = super_amount THEN @rank_count\
-                WHEN @prev_value := super_amount THEN @rank_count := @rank_count + 1\
-                END AS rank\
-            FROM (\
-                    SELECT pii.title, pii.subtitle, sum(ocg.amount) as super_amount\
-                    FROM orders_contain_games ocg\
-                        JOIN game g ON g.ean_number = ocg.ean_number\
-                        JOIN platform_independent_info pii ON g.pi_id = pii.pi_id\
-                    GROUP BY pii.title\
-                    ORDER BY super_amount DESC\
-            ) as ocgt\
-        ) as ranked\
-        WHERE ranked.rank <= 10"
+        // var cur_day = cur_date.getDate();
+        // var cur_month = cur_date.getMonth() + 1;
+        // var cur_year = cur_date.getFullYear();
+
+        // cur_day = putZeroBeforeNum(cur_day);
+        // cur_
+
+        // var end_date = "'" + cur_year + "-" + cur_month + "-" + cur_day + "'";
+
+
+        // var begin_date = cur_date;
+        // begin_date.setMonth(cur_date.getMonth() - 3);
+
+        // begin_date = "'" + begin_date.getYear() + "-" + begin_date.getMonth() + "-" + cur_day.getDate() + "'";
+
+
+
 
         var hoogst_aantal_users_qry =
        "SET @prev_value = NULL;\
@@ -87,6 +67,13 @@ Stats.init = function(server, database)
  
         next(); 
     }); 
+
+    function putZeroBeforeNum (num) {
+        if (parseInt(num) < 10) {
+            num = "0" + num;
+            console.log(num);
+        }
+    }
 }
 
 module.exports = function (server, database)
