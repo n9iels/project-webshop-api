44b19fc9f0a450eb2beeab7bb6c5300c3e069d2d
DOverbeeke <donaldoverbeeke@hotmail.com>
2017-02-01 01:46:15 +0100

    #30 all charts working! mobile version pretier+see issues

--- endpoints/stats.js
+++ endpoints/stats.js
@@ -51,7 +51,7 @@ Stats.init = function(server, database, Authenticate)
         next(); 
     })
 
-    // Endpoint for '/stats/topgames' to get games bought by most users
+    // Endpoint for '/stats/rev' to get revenue per day
     server.get('stats/rev', function (req, res, next) // NOTICE: 'stats/:month'
     { 
         var b_date = req.query.b_date;
@@ -72,7 +72,34 @@ Stats.init = function(server, database, Authenticate)
             }
             else if (result.length > 0) 
             { 
-                res.send(result); //without the [2] it sends three objects: two empty ones and the third (result[2]) containing the games
+                res.send(result);
+            } 
+            else 
+            { 
+                res.send(404, "result.length !> 0"); 
+            } 
+        }) 
+ 
+        next(); 
+    })
+
+    // Endpoint for '/stats/useramnt' to get user amount per day
+    server.get('stats/useramnt', function (req, res, next) // NOTICE: 'stats/:month'
+    { 
+        var b_date = req.query.b_date;
+        var e_date = req.query.e_date;
+
+        var useramnt_qry = "SELECT registration_date FROM `user` WHERE registration_date BETWEEN '1900-01-01' AND '" + e_date + "' ORDER BY registration_date";
+
+        database.executeQuery(useramnt_qry, [], function(result, error)
+        {
+            if (error)
+            {
+                res.send(500, error)
+            }
+            else if (result.length > 0) 
+            { 
+                res.send(result);
             } 
             else 
             { 
